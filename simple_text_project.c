4ebb558a2ea14ed6dbae20292c1508ae10216d5a
bmonagan <bernardmonagan@gmail.com>
2025-10-24 13:48:56 -0400

    part 2. bit of a dumb project

--- simple_text_project.c
+++ simple_text_project.c
@@ -4,7 +4,7 @@
 int main() {
     // TODO: Write your code here
     // 1. Print the prompt message
-    printf("Enter a sentence: \n");
+    printf("Enter a sentence: ");
     // 2. Declare a character array named 'sentence' with 200 elements
     char sentence[200];
     // 3. Read input using scanf with %s format specifier
@@ -12,6 +12,7 @@ int main() {
     // 4. Print the entered word and its length
     printf("You entered: %s\n", sentence);
     int length = strlen(sentence);
+    printf("Character count: %d\n", length);
     printf("Length: %d", length);
     
     return 0;
