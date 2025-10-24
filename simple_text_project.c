9880d6d8e04df6d830afc65391c9d1887fb99d22
bmonagan <bernardmonagan@gmail.com>
2025-10-24 13:44:09 -0400

    simple_text_project: Initial commit

---
+++ simple_text_project.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    // TODO: Write your code here
+    // 1. Print the prompt message
+    printf("Enter a sentence: \n");
+    // 2. Declare a character array named 'sentence' with 200 elements
+    char sentence[200];
+    // 3. Read input using scanf with %s format specifier
+    scanf("%s",&sentence);
+    // 4. Print the entered word and its length
+    printf("You entered: %s\n", sentence);
+    int length = strlen(sentence);
+    printf("Length: %d", length);
+    
+    return 0;
+}
