4f7bf9edf9c67f9789b44a9c22d806212110d060
bmonagan <bernardmonagan@gmail.com>
2025-10-24 14:00:18 -0400

    simple text 3

--- simple_text_project.c
+++ simple_text_project.c
@@ -1,19 +1,33 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h> // Required for tolower()
+
+int is_vowel(char c){
+    c = tolower(c);
+    return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+}
 
 int main() {
     // TODO: Write your code here
     // 1. Print the prompt message
-    printf("Enter a sentence: ");
+    printf("Enter a sentence: \n");
     // 2. Declare a character array named 'sentence' with 200 elements
     char sentence[200];
+    int vowel_count = 0;
     // 3. Read input using scanf with %s format specifier
     scanf("%s",&sentence);
     // 4. Print the entered word and its length
     printf("You entered: %s\n", sentence);
     int length = strlen(sentence);
     printf("Character count: %d\n", length);
-    printf("Length: %d", length);
-    
+    printf("Length: %d\n", length);
+    for (int i = 0; sentence[i] != '\0'; i++){
+        // Print the character being processed
+        
+        if (is_vowel(sentence[i])){
+            vowel_count++;
+        }
+    }
+    printf("Vowel count: %d",vowel_count);
     return 0;
 }
